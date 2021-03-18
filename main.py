acab5351e3b4b06634d9e98d5a1939ca0ac91e1f
Dani Lerner <dlerner97@gmail.com>
2021-03-18 18:57:11 -0400

    Dijikstra changes

--- main.py
+++ main.py
@@ -1,129 +1,23 @@
 import os
 import cv2
-import math
-import numpy as np
-from BFS import BFSSearch
+import search
+import trial_maps as maps
 
-# Parent: BFSSearch
-# This class builds the trial map and runs the BFS algorithm on it
-class TrialMap(BFSSearch):
-    def __init__(self, scale_percent=100, add_frame_frequency=100, framerate=100):
-        # Build grid
-        self.grid, self.height, self.width = TrialMap.gen_grid()
-        
-        # Initialize BFS Search
-        super().__init__("trial_map", (15, 50), (185, 50), scale_percent, add_frame_frequency, framerate)
-    
-    # Build grid with red obstacles
-    @staticmethod
-    def gen_grid():
-        width = 200
-        height = 100
-        
-        grid = np.zeros((height,width, 3), np.uint8)                # Initialize shape
-        grid[40-1: 60, 90-1:110] = (0,0,255)                        # Rectangle
-        center_circ = (width-1-40, 50-1)
-        cv2.circle(grid, center_circ, 15, (0,0,255), -1)            # Circle
-        return grid[::-1,:], height, width        
-    
-# Parent: BFSSearch
-# This class builds the submission map and runs the BFS algorithm on it    
-class SubmissionMap(BFSSearch):
-    def __init__(self, scale_percent=100, add_frame_frequency=100, framerate=100) -> None:
-        self.grid, self.height, self.width = SubmissionMap.gen_grid()
-        super().__init__("submission_map", (15, 150), (385, 150), scale_percent, add_frame_frequency, framerate)
-    
-    # Build grid with red obstacles
-    @staticmethod
-    def gen_grid():
-        width = 400
-        height = 300
-        grid = np.zeros((height, width, 3), np.uint8)                # Initialize shape
-        
-        # Circle
-        center_circ = (90-1, 70-1)
-        cv2.circle(grid, center_circ, 35, (0,0,255), -1)
-        
-        # Utility lambda functions
-        deg2rad = lambda deg: deg*math.pi/180
-        int_ = lambda val: int(round(val))
-        
-        # Rectangle
-        lower_left_corner = [48-1, 108-1]
-        angle = deg2rad(35)
-        upper_left_corner = [int_(lower_left_corner[0] + 20*math.cos(angle+deg2rad(90)))-1, int_(lower_left_corner[1] + 20*math.sin(angle+deg2rad(90)))-1]
-        lower_right_corner = [int_(lower_left_corner[0] + 150*math.cos(angle))-1, int_(lower_left_corner[1] + 150*math.sin(angle))-1]
-        upper_right_corner = [int_(upper_left_corner[0] + 150*math.cos(angle))-1, int_(upper_left_corner[1] + 150*math.sin(angle))-1]        
-        pts = np.array([upper_left_corner, lower_left_corner, lower_right_corner, upper_right_corner], np.int32).reshape((-1,1,2))
-        cv2.fillPoly(grid, [pts], (0,0,255))
-        
-        # Ellipse
-        center_ell = (246-1, 145-1)
-        cv2.ellipse(grid, center_ell, (120//2, 60//2), 0, 0, 360, (0,0,255), -1)
-        
-        # Top polygon
-        left_x = 200-1
-        top_y = height-1-20
-        bot_y = top_y-50
-        top_left = [left_x, top_y]
-        bot_left = [left_x, bot_y]
-        bot_right = [left_x+30, bot_y]
-        bot_mid_right = [left_x+30, bot_y+10]
-        bot_mid = [left_x+10, bot_y+10]
-        top_mid = [left_x+10, bot_y+40]
-        top_mid_right = [left_x+30, bot_y+40]
-        top_right = [left_x+30, top_y]
-        pts = np.array([top_left, bot_left, bot_right, bot_mid_right, bot_mid, top_mid, top_mid_right, top_right], np.int32).reshape((-1,1,2))
-        cv2.fillPoly(grid, [pts], (0,0,255))
-        
-        # Right polygon
-        angle_low = deg2rad(45)
-        bot = [width-1-72, 63-1]
-        right_x = bot[0] + int_(75*math.cos(angle_low))
-        right_bot_y = bot[1] + int_(75*math.sin(angle_low))
-        mid_right = [right_x, right_bot_y]
-        top_right = [right_x, right_bot_y+55]
-        left = [bot[0]+int_(60*math.cos(angle_low+deg2rad(90))), bot[1]+int_(60*math.sin(angle_low+deg2rad(90)))]
-        top_mid = [left[0]+int_(60*math.cos(angle_low)), left[1]+int_(60*math.sin(angle_low))]
-        bot_mid = [354-1, 138-1]
-        pts = np.array([bot, mid_right, top_right, bot_mid, top_mid, left], np.int32).reshape((-1,1,2))
-        cv2.fillPoly(grid, [pts], (0,0,255))
-            
-        return grid[::-1,:], height, width  
-  
-# Parent: BFSSearch
-# This class builds a map with no path between the left and right-hand sides.
-# It displays the the algorithms ability to understand that no path is possible      
-class TestNoPathMap(BFSSearch):
-    def __init__(self, scale_percent=100, add_frame_frequency=100, framerate=100):
-        self.grid, self.height, self.width = TestNoPathMap.gen_grid()
-        super().__init__("test_no_possible_path", (50, 50), (150, 50), scale_percent=scale_percent, add_frame_frequency=add_frame_frequency, framerate=framerate)
-    
-    # Build grid with red obstacles
-    @staticmethod
-    def gen_grid():
-        width = 200
-        height = 100
-        
-        grid = np.zeros((height,width, 3), np.uint8)                # Initialize shape
-        grid[:, 90:100] = (0,0,255)
-        
-        return grid[::-1,:], height, width
-        
-        
-        
 if __name__ == '__main__':
     os.system('cls')
 
-    trial_map = TrialMap(scale_percent=300,add_frame_frequency=50, framerate=100)
+    trial_map = search.DijkstraSearch(maps.TrialMap, scale_percent=300,add_frame_frequency=50, framerate=100)
     trial_map.find_path()
     
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
+    
+    # trial_map = search.BFSSearch(maps.TrialMap, scale_percent=300,add_frame_frequency=50, framerate=100)
+    # trial_map.find_path()
 
-    submission_map = SubmissionMap(scale_percent=200, add_frame_frequency=300, framerate=100)
-    submission_map.find_path()
+    # submission_map = SubmissionMap(scale_percent=200, add_frame_frequency=300, framerate=100)
+    # submission_map.find_path()
     
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
     
-    test_no_path_map = TestNoPathMap(scale_percent=300, add_frame_frequency=50, framerate=100)
-    test_no_path_map.find_path()
+    # test_no_path_map = TestNoPathMap(scale_percent=300, add_frame_frequency=50, framerate=100)
+    # test_no_path_map.find_path()
---
+++ search.py
@@ -0,0 +1,371 @@
+import cv2
+import time
+import math
+import queue
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+
+# This class handles simple image and video tasks
+class VideoBuffer():
+    def __init__(self, framerate = 100, scale_percent=100):
+        self.frames = []
+        self.framerate = framerate
+        self.scale_percent = scale_percent
+      
+    # Resizes each length image at a given percent. E.g. 200 will double each dimension and 50 will half it
+    @staticmethod
+    def resize_image(grid, scale_percent=100):
+        width = int(grid.shape[1] * scale_percent / 100)
+        height = int(grid.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        return cv2.resize(grid, dim, interpolation = cv2.INTER_AREA) 
+      
+    # Displays grid at a given scale
+    @staticmethod
+    def show_grid(grid, scale_percent=100, wait=0):    
+        resized = VideoBuffer.resize_image(grid, scale_percent) 
+        # plt.imshow(grid, "gray")
+        # plt.show()
+        cv2.imshow("grid",resized)
+        k = cv2.waitKey(wait) & 0xff
+        if k == ord('q'):
+           exit()
+           
+    # Write frames to feed and save video
+    def save(self, video_name, isColor = False):
+        shape = self.frames[0].shape
+        size = (shape[1], shape[0])
+        videowriter = cv2.VideoWriter(video_name + ".avi", cv2.VideoWriter_fourcc(*'DIVX'),self.framerate , size, isColor)
+        for f in self.frames:
+            videowriter.write(f)
+        videowriter.release()
+
+# Parent: VideoBuffer
+# Child: Actual Search Algorithm
+# This class handles the general search implementation. To run this code, however 
+class Search(VideoBuffer):
+    # Takes in video_name, default start/goal positions, output feed scale factor, number of nodes between subsequent frames, video framerate
+    def __init__(self, map, cost_algorithm=False, scale_percent=100, add_frame_frequency=100, framerate = 100):
+        # Init VideoBuffer Parent
+        super().__init__(framerate=framerate, scale_percent=scale_percent)
+        print("\n=====================================================================================\n")
+        
+        # Define map
+        self.video_name, self.grid, self.height, self.width = map.gen_grid()
+        start_pos_def = (10, self.height//2)
+        goal_pos_def = (self.width-10, self.height//2)
+        
+        # Query user for positions
+        def get_user_input(pos_string, default):            
+            # Query until user has input legal values     
+            while True:
+                start_str = input(f"Enter {pos_string} position as x,y (0 <= x <= {self.width-1} ',' and 0 <= y <= {self.height-1}) or leave blank to apply the default values: ")
+                start_str_nw = start_str.replace(" ", "")
+                
+                # If empty string, set values to default
+                if start_str_nw == '':
+                    print("Selecting default values")
+                    return default              
+                    
+                start_str_list = start_str_nw.split(",")
+                
+                # Check for incorrect input
+                try:
+                    x_pos = int(start_str_list[0])
+                    y_pos = self.height-1-int(start_str_list[1])
+                except:
+                    print("Please type two integers with a comma in between.\n")
+                    continue    
+                
+                # Check if position out of bounds
+                if x_pos < 0 or x_pos >= self.width or y_pos < 0 or y_pos >= self.height:
+                    print("Numbers out of bounds. Please select new position.\n")
+                    continue
+                if np.all(self.grid[y_pos, x_pos] == (0,0,255)):
+                    print("There is an obstacle here. Please select a new position.\n")
+                    continue
+                
+                # User chose correct inputs
+                return (x_pos, y_pos)
+        
+        # Define start/goal positions
+        self.start_pos = get_user_input("start", start_pos_def)
+        print("")
+        self.goal_pos = get_user_input("goal", goal_pos_def)
+        print("\n=====================================================================================")
+        
+        print("\nSelect image window and press any key to begin. Regardless, code will run in 10 seconds\nHit 'ctrl+C' at any time to quit execution.")
+        
+        # Add start/goal positions to grid
+        temp_grid = self.grid.copy()
+        cv2.circle(temp_grid, self.start_pos, 3, (0,255,0), -1)
+        cv2.circle(temp_grid, self.goal_pos, 3, (0,255,0), -1)
+        self.grid = temp_grid
+        
+        # Display grid
+        self.scale_percent = scale_percent
+        Search.show_grid(self.grid, scale_percent, wait=10000)
+        cv2.destroyAllWindows()
+        self.frames.append(self.grid)
+        
+        # Define variables
+        self.current_set = queue.Queue()                                                # Queue of states to inspect 
+        orig_parent_loc_string = str(self.start_pos[0]) + " " + str(self.start_pos[1])
+        if cost_algorithm:
+            self.node_info = {orig_parent_loc_string : {"cost_from_first": 0}}          # Dictionary of node info
+            self.current_set.put_nowait((self.start_pos, 0))
+        else:
+            self.node_info = {orig_parent_loc_string : {}}          # Dictionary of node info
+            self.current_set.put_nowait(self.start_pos)
+                                                           
+        self.add_frame_frequency = round(add_frame_frequency/8)        
+        self.uint8_0 = np.uint8(0)                                                       # Define a 0 as an 8-bit unsigned int. This will save processing time    
+          
+    # Convert position tuple into string for dictionary storage
+    @staticmethod
+    def pos2str(pos):
+        return str(pos[0]) + " " + str(pos[1]) 
+        
+    # Protected method for searching the current current queue and generate all depth levels. 
+    # This method and the _find_path method are protected since they can not be run alone. This is a general method and does not implement any specific search algorithm.
+    # Rather, it should be absorbed by an actual search algorithm (BFS, Dijkstra, etc.). True search algorithm MUST have a method called "_check_subsequent nodes"
+    #   and store nodes to the proper queue  
+    def _find_path(self):
+        print("\nFinding Path...")
+        start_time = time.time()                                # Time path planning
+        solution_found = False
+        count = 0
+        c1 = 0
+        try:
+            while (not solution_found):
+                
+                # If queue is empty, stop
+                if self.current_set.empty():
+                    break
+                
+                # Pull state from queue and check subsequent nodes
+                parent = self.current_set.get_nowait()
+                solution_found = self._check_subsequent_nodes(parent)
+                
+                # Only add frame to video at specified intervals
+                count += 1
+                c1 += 1
+                if count == self.add_frame_frequency:
+                    self.frames.append(self.grid.copy())
+                    count = 0
+                # if c1 == 20:
+                #     self.show_grid(self.grid, 300)
+                #     exit()
+            
+            print("\n=====================================================================================")
+            if solution_found:
+                # Go up the dictionary tree to find all parent nodes and optimized path
+                next_set = self.node_info[Search.pos2str(self.goal_pos)]
+                str_list = Search.pos2str(self.goal_pos).split(" ")
+                num_list = [int(str_list[0]), int(str_list[1])]
+                path = [num_list]
+                try:
+                    while True:
+                        print('-'*10)
+                        next_set_ind = next_set['parent']
+                        next_set = self.node_info[next_set_ind]
+                        
+                        # Parse strings and add them to path
+                        str_list = next_set_ind.split(" ")
+                        num_list = (int(str_list[0]), int(str_list[1]))
+                        path.append(num_list)
+                        
+                        # print(num_list)
+                        # for move in self.action_set.values():
+                        #     try:    
+                        #         print('-'*3)
+                        #         pos = tuple(map(operator.add, move['move'], num_list))
+                        #         print(move['move'], pos)
+                        #         node_info = self.node_info[Search.pos2str(pos)]
+                        #         print(node_info)
+                        #         print('tot cost:', node_info['cost_from_first']+move['cost'])
+                        #     except:
+                        #         continue
+                            
+                        # Change grid color and add frames to video
+                        self.grid[num_list[1], num_list[0]] = (0,255,0)
+                        self.frames.append(self.grid.copy())
+                        # self.show_grid(self.grid, 300)
+                        print('-'*10)
+                except KeyError:
+                    pass
+
+                # Print full move set
+                path.reverse()
+                self.path = np.asarray(path)
+                self.grid[self.path[:, 1], self.path[:, 0]] = (0,255,0)
+                
+                # Calculate time elapsed
+                time_elapsed_s = time.time() - start_time
+                time_elapsed_mins = time_elapsed_s//60
+                time_elapsed_hrs = time_elapsed_s//60**2
+                time_elapsed_secs = time_elapsed_s%60
+                
+                print(f"\nThis path can be solved in {len(self.path)-1} moves")
+                print(f"The BFS algorithm took {time_elapsed_hrs} hrs, {time_elapsed_mins} mins, and {time_elapsed_secs} s to implement.\n")
+              
+            # If no solution was found
+            else:
+                print("\nThe BFS algorithm cannot detect a path. Please select new start and goal points or remove obstacles from the grid.")
+               
+            # Save and write video feed
+            self.save(self.video_name + "_animation", True)
+            print(f"The video file can be found in your current directory under {self.video_name}_animation.avi")
+            print("\n=====================================================================================\n")
+
+            # Display grid
+            Search.show_grid(self.grid, self.scale_percent, wait=10000)   
+               
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt")
+
+# The BFSSearch class is a wrapper for search.            
+class BFSSearch(Search):
+    def __init__(self, map, scale_percent, add_frame_frequency, framerate):
+        super().__init__(map, scale_percent=scale_percent, add_frame_frequency=add_frame_frequency, framerate=framerate)
+        self.action_set = {
+            "up"         :  (0,-1),
+            "up_right"   :  (1,-1),
+            "right"      :   (1,0),
+            "down_right" :   (1,1),
+            "down"       :   (0,1),
+            "down_left"  :  (-1,1),
+            "left"       :  (-1,0),
+            "up_left"    : (-1,-1)
+        }
+    
+    def _get_next_branch(self, parent, from_dir):  
+        # Get the new position using tuple addition
+        pos = tuple(map(operator.add, parent, (from_dir)))
+
+        # Check if new position is on the grid
+        on_grid =             pos[1] < self.height
+        on_grid = on_grid and pos[1] >= 0
+        on_grid = on_grid and pos[0] < self.width
+        on_grid = on_grid and pos[0] >= 0 
+        
+        # Check if move is legal and generate that new state
+        if (on_grid):
+            loc_string = Search.pos2str(pos) 
+            parent_loc_string = Search.pos2str(parent)
+            
+            # If position corresponds to the winning state, return True
+            if pos[0] == self.goal_pos[0] and pos[1] == self.goal_pos[1]:
+                self.node_info[loc_string] = {"parent": parent_loc_string}
+                return True
+            
+            # Check if the node has already been visited or if it is on an obstacle
+            color = self.grid[pos[1], pos[0]]
+            if self.node_info.get(loc_string) == None and color[-1] == self.uint8_0:
+                
+                # Change pixel color to blue unless it overlaps with the start and end depictions
+                if color[1] == self.uint8_0:
+                    self.grid[pos[1], pos[0]] = (255,0,0)
+                
+                # Update queue and parent info
+                self.node_info[loc_string] = {"parent": parent_loc_string}
+                self.current_set.put_nowait(pos)
+        return False
+    
+    # Check all subsequent directions for a given position
+    def _check_subsequent_nodes(self, parent):
+
+        # Check all directions
+        found_goal = False
+        for dir in self.action_set.values(): 
+            found_goal = self._get_next_branch(parent, dir) or found_goal
+        
+        # If any of these are true, we have reached the goal state. Propogate the true to the next level
+        return found_goal
+    
+    def find_path(self):
+        self._find_path()
+
+class DijkstraSearch(Search):
+    def __init__(self, map, scale_percent, add_frame_frequency, framerate):
+        super().__init__(map, cost_algorithm=True, scale_percent=scale_percent, add_frame_frequency=add_frame_frequency, framerate=framerate)
+        self.action_set = {
+            "up"         : {"move":  (0,-1), "cost": 1},
+            "up_right"   : {"move":  (1,-1), "cost": math.sqrt(2)},
+            "right"      : {"move":   (1,0), "cost": 1},
+            "down_right" : {"move":   (1,1), "cost": math.sqrt(2)},
+            "down"       : {"move":   (0,1), "cost": 1},
+            "down_left"  : {"move":  (-1,1), "cost": math.sqrt(2)},
+            "left"       : {"move":  (-1,0), "cost": 1},
+            "up_left"    : {"move": (-1,-1), "cost": math.sqrt(2)},
+        }
+    
+    def _get_next_branch(self, parent, from_dir, branch_cost):  
+        # Get the new position using tuple addition
+        parent_cost = parent[1]
+        parent = parent[0]
+        pos = tuple(map(operator.add, parent, (from_dir)))
+        
+        # Check if new position is on the grid
+        on_grid =             pos[1] < self.height
+        on_grid = on_grid and pos[1] >= 0
+        on_grid = on_grid and pos[0] < self.width
+        on_grid = on_grid and pos[0] >= 0 
+        
+        # Check if move is legal and generate that new state
+        if (on_grid):
+            loc_string = Search.pos2str(pos) 
+            parent_loc_string = Search.pos2str(parent)
+            cost_from_first = parent_cost+branch_cost
+            
+            # If position corresponds to the winning state, return True
+            if pos[0] == self.goal_pos[0] and pos[1] == self.goal_pos[1]:
+                self.node_info[loc_string] = {"parent": parent_loc_string, "cost_from_first": cost_from_first}
+                return True 
+                       
+            # Check if the node is on an obstacle
+            color = self.grid[pos[1], pos[0]]
+            if color[-1] == self.uint8_0:
+                # Check if the node has already been visited
+            
+                print("-"*10) 
+                print(f"pos: {pos}, parent: {parent}")
+                print(f"branch cost: {branch_cost}, parent cost: {parent_cost}, total cost: {cost_from_first}")
+                
+                node_info = self.node_info.get(loc_string)
+                print(f"Node info: {node_info}")
+                
+                if node_info == None:
+                    # Change pixel color to blue unless it overlaps with the start and end depictions
+                    if color[1] == self.uint8_0:
+                        self.grid[pos[1], pos[0]] = (255,0,0)
+                    
+                    # Update queue and parent info
+                    self.node_info[loc_string] = {"parent": parent_loc_string, "cost_from_first": cost_from_first}
+                    self.current_set.put_nowait((pos, cost_from_first))
+                    print(self.node_info[loc_string])
+                
+                elif cost_from_first < node_info["cost_from_first"]:
+                    # print('-'*10)
+                    # print(pos)
+                    # print(self.node_info[loc_string])
+                    self.node_info[loc_string] = {"parent": parent_loc_string, "cost_from_first": cost_from_first}
+                    self.grid[pos[1], pos[0]] = (255,255,0)
+                    print(self.node_info[loc_string])
+                    # print('-'*10)
+                    
+        return False
+        
+    def _check_subsequent_nodes(self, parent):
+        # Check all directions
+        found_goal = False
+        print("\n", "="*10, sep="")
+        for branch_info in self.action_set.values(): 
+            found_goal = self._get_next_branch(parent, branch_info['move'], branch_info['cost']) or found_goal
+        
+        # If any of these are true, we have reached the goal state. Propogate the true to the next level
+        return found_goal
+    
+    def find_path(self):
+        self._find_path()
